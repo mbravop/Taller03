e13677e64630f17d342dfd08c76bd281eb05b47a
Mauricio Bravo <82302737+mbravop@users.noreply.github.com>
2022-10-26 17:26:55 -0500

    SolucionClaseCompra

---
+++ SolucionClaseCompra.java
@@ -0,0 +1,16 @@
+public class Compra {
+    private Pago pago;
+    private List articulos;
+
+    public Compra(Pago pago){
+        //inicializaciones
+    }
+
+    public void agregarArticulo(Articulo articulo){
+        //agregar un artículo a la compra
+    }
+
+    public void removerArticulo(Articulo articulo){
+        //remover un artículo a la compra
+    }
+}
