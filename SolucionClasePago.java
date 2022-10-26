4a968482a2ad0a5450b6beb65deb24999d2c3ae3
Mauricio Bravo <82302737+mbravop@users.noreply.github.com>
2022-10-26 17:24:56 -0500

    SolucionClasePago

---
+++ SolucionClasePago.java
@@ -0,0 +1,19 @@
+public class Pago{
+    //atributos
+    public void realizarCobro(double monto){
+        //Cargar el monto de compra al medio de pago
+    }
+
+    public void generarFactura(){
+        //Generar una nueva factura
+    }
+}
+
+
+public class Impuesto {
+    //atributos
+    public void calcularImpuestosFactura(Compra compra){
+        //Calcula los impuestos asociados a la compra
+    }
+}
+
